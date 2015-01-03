f68fcfc2e7cbafba1e7c0592fa418628f5b4dca7
Thomas Guerin <adminmizix@gmail.com>
2015-01-03 15:53:48 +0100

    RMIWebServices ok

--- src/sellingBook/MainServerLibraries.java
+++ src/sellingBook/MainServerLibraries.java
@@ -1,5 +1,6 @@
 package sellingBook;
 import java.net.MalformedURLException;
+import java.nio.file.FileSystem;
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
@@ -12,7 +13,9 @@ public class MainServerLibraries {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException {
 		System.out.println("Lancement du server de librairies");
-	    System.out.println("Working Directory = " + System.getProperty("user.dir"));
+	    System.out.println("Working Directory = " + System.getProperty("java.class.path"));
+	   
+	   
 		System.setProperty("java.security.policy", "sec.policy");
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new RMISecurityManager());
